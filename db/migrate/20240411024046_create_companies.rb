9cd3b3f108a22744e17b2a491506dd7689518af5
Ishanvaid1995 <ishanvaid95@gmail.com>
2024-04-12 13:15:28 -0400

    Sqlite to Postgres Migration

--- db/migrate/20240411024046_create_companies.rb
+++ db/migrate/20240411024046_create_companies.rb
@@ -1,11 +1,11 @@
 class CreateCompanies < ActiveRecord::Migration[7.0]
   def change
     create_table :companies do |t|
-      t.string :name
-      t.string :status
+      t.text :name
+      t.text :status
       t.integer :facilities
       t.integer :users
-      t.string :logo
+      t.text :logo
 
       t.timestamps
     end
